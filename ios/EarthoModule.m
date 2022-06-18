dc4e9d522dbe27b4a0b696a1f678424f1413d292
dvir <dvir.daniel@hotmail.com>
2022-06-18 18:50:34 +0300

    1.0.0

--- ios/EarthoModule.m
+++ ios/EarthoModule.m
@@ -10,4 +10,17 @@ RCT_EXTERN_METHOD(initEartho:(NSString)clientId
 RCT_EXTERN_METHOD(connectWithRedirect:(NSString)accessId
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(getUser:(NSString)a
+                withResolver:(RCTPromiseResolveBlock)resolve
+                withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(getIdToken:(NSString)a
+                withResolver:(RCTPromiseResolveBlock)resolve
+                withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(disconnect:(NSString)a
+                withResolver:(RCTPromiseResolveBlock)resolve
+                withRejecter:(RCTPromiseRejectBlock)reject)
+
 @end
--- ios/EarthoModule.swift
+++ ios/EarthoModule.swift
@@ -17,25 +17,85 @@ class EarthoModule: NSObject {
     @objc(connectWithRedirect:withResolver:withRejecter:)
     func connectWithRedirect(
         accessId: String, 
-        resolve:RCTPromiseResolveBlock,
-        reject:RCTPromiseRejectBlock) -> Void {
+        resolve: @escaping RCTPromiseResolveBlock,
+        reject:@escaping RCTPromiseRejectBlock) -> Void {
                  let earthoOne = EarthoOne()
-earthoOne.connectWithPopup(
+            earthoOne.connectWithPopup(
             accessId: accessId,
             onSuccess: { credentials in
-                // do {
-                //     let data = EarthoCredentials( tokenType: credentials.tokenType, expiresIn: credentials.expiresIn, refreshToken: credentials.refreshToken, idToken: credentials.idToken, scope: credentials.scope, recoveryCode: credentials.recoveryCode
-                //     )
-                //     let encoder = JSONEncoder()
-                //     let json = try encoder.encode(data)
-                //     let result = String(data: json, encoding: .utf8)!
-                //     flutterResult(result)
-                // } catch {
-                //     flutterResult(FlutterError(code: "ConnectFailure", message: "Failure Logging In With EarthoOne", details: "Error encoding credentials"))
-                // }
+                 do {
+                     let data = EarthoCredentials( tokenType: credentials.tokenType, expiresIn: credentials.expiresIn, refreshToken: credentials.refreshToken, idToken: credentials.idToken, scope: credentials.scope, recoveryCode: credentials.recoveryCode
+                     )
+                     let encoder = JSONEncoder()
+                     let json = try encoder.encode(data)
+                     let result = String(data: json, encoding: .utf8)!
+                     resolve(result)
+                 } catch {
+                     reject("ConnectFailure", "Failure Connect With EarthoOne", nil)
+                 }
             },
             onFailure: { WebAuthError in
-                // flutterResult(FlutterError(code: "ConnectFailure", message: "Failure connecting with EarthoOne", details: WebAuthError))
+                reject("ConnectFailure", "Failure Connect With EarthoOne", WebAuthError)
             })
     }
+
+    @objc(getIdToken:withResolver:withRejecter:)
+    func getIdToken(
+        a:String,
+        resolve:RCTPromiseResolveBlock,
+        reject:RCTPromiseRejectBlock) -> Void {
+    
+            resolve(earthoOne.getIdToken())
+    }
+
+    @objc(getUser:withResolver:withRejecter:)
+    func getUser(
+        a:String,
+        resolve:RCTPromiseResolveBlock,
+        reject:RCTPromiseRejectBlock) -> Void {
+        
+            do {
+                       let credentials = earthoOne.getUser();
+                       guard credentials != nil else {
+                           resolve(nil)
+                           return
+                       }
+
+                       let dataa = UserResult(uid: credentials?.uid as! String, displayName: credentials?.displayName as? String, email: credentials?.email as? String, photoURL: credentials?.photoURL as? String, firstName: credentials?.firstName as? String, lastName: "", phone: "")
+                       let encoder = JSONEncoder()
+                       let json = try encoder.encode(dataa)
+                       let result = String(data: json, encoding: .utf8)!
+                resolve(result)
+                   } catch {
+                       reject("GetUserFailure", "Failure Get User Data", error)
+                   }
+    }
+
+    @objc(disconnect:withResolver:withRejecter:)
+    func disconnect(
+        a:String,
+        resolve:RCTPromiseResolveBlock,
+        reject:RCTPromiseRejectBlock) -> Void {
+        earthoOne.logout();
+        resolve("")
+    }
+    
+    struct EarthoCredentials: Codable {
+           var tokenType: String?
+           var expiresIn: Date?
+           var refreshToken: String?
+           var idToken: String?
+           var scope: String?
+           var recoveryCode: String?
+       }
+       
+       struct UserResult: Codable {
+           var uid: String?
+           var displayName: String?
+           var email: String?
+           var photoURL: String?
+           var firstName: String?
+           var lastName: String?
+           var phone: String?
+       }
 }
